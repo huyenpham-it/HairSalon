55d1f961f6bd31a83a42cfc0e7c1d8a70772a071
huyenpham-it <dieuhuyenpham.it@gmail.com>
2025-11-08 21:38:19 +0700

    add constants.js

---
+++ ultis/constants.js
@@ -0,0 +1,191 @@
+export const SERVICE_SELECT = [
+	{ id: 1, service: 'Hair Styling', link: '/services/hair-styling' },
+	{ id: 2, service: 'Brows & Lashes', link: '#' },
+	{ id: 3, service: 'Nail Art', link: '#' },
+	{ id: 4, service: 'Makeup', link: '#' },
+	{ id: 5, service: 'Hair Color', link: '#' },
+	{ id: 6, service: 'Men’s Services', link: '#' }
+];
+
+export const OUR_PAGES = [
+	{ id: 1, page: 'Appointment', link: '#' },
+	{ id: 2, page: 'About Us', link: '#' },
+	{ id: 3, page: 'Our Team', link: '#' },
+	{ id: 4, page: 'Shop', link: '/shop' },
+	{ id: 5, page: 'Image Credits', link: '#' },
+];
+
+export const HOME_SLIDER_FOUR = [
+	{
+		src: '/images/52-service-1.webp',
+		link: '#',
+		title: 'Hair styling',
+		description: 'Get a fresh look for your hair with our talented hairstylists.',
+		price: '$80'
+	},
+	{
+		src: '/images/52-service-4.webp',
+		link: '#',
+		title: 'Makeup',
+		description: 'Try out a whole new look with makeup services.',
+		price: '$80'
+	},
+	{
+		src: '/images/52-service-3.webp',
+		link: '#',
+		title: 'Nail art',
+		description: 'Let your nails be a reflection of your unique style and personality.',
+		price: '$55'
+	},
+	{
+		src: '/images/52-service-6.webp',
+		link: '#',
+		title: 'Hair color',
+		description: 'Give yourself a new look with hair color services.',
+		price: '$125'
+	},
+	{
+		src: '/images/52-service-5.webp',
+		link: '#',
+		title: 'Men’s services',
+		description: 'We provide our male clientele with the latest in men’s cuts, styles, and hair color.',
+		price: '$65'
+	},
+	{
+		src: '/images/52-service-2.webp',
+		link: '#',
+		title: 'Brows & Lashes',
+		description: 'Highlight your eyes and shape your brows with our brow and lash experts.',
+		price: '$30'
+	}
+];
+
+export const HOME_FOUR_IMAGES = [
+	{
+		src: '/images/52-profile-1.webp',
+		name: 'Alison Cooper',
+		job: 'Makeup Artist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-2.webp',
+		name: 'Christie Weiss',
+		job: 'Hair Stylist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-3.webp',
+		name: 'Rebecca Smith',
+		job: 'Lash & Brow Artist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-4.webp',
+		name: 'Joan Robins',
+		job: 'Nail Artist',
+		link: '#'
+	}
+];
+
+export const HOME_SLIDER_SEVEN = [
+	{
+		src: '/images/52-quote-1.webp',
+		name: 'Harvey J.',
+		comment:
+			'First of all, a gorgeous salon! Such cute & trendy interior and a very laid back vibe. Dani did my highlights & it was exactly what I wanted!'
+	},
+	{
+		src: '/images/52-quote-2.webp',
+		name: 'Mia T.',
+		comment:
+			'Love this salon! I get my facials done by grace. The environment is so relaxing and i recommend it to anyone i talk to.'
+	},
+	{
+		src: '/images/52-quote-3.webp',
+		name: 'Kate p.',
+		comment:
+			'Customer service and Cosmetic/Spa services are amazing! Love all the employees, great vibe walking in. I highly recommend!'
+	}
+];
+
+export const HOME_SLIDER_EIGHT = [
+	{
+		src: '/images/52-home-gallery-1.webp',
+		description: 'Hair coloring by Amanda'
+	},
+	{
+		src: '/images/52-home-gallery-2.webp',
+		description: 'Nail art by Rebecca'
+	},
+	{
+		src: '/images/52-home-gallery-3.webp',
+		description: 'Hair styling by Jerry'
+	},
+	{
+		src: '/images/52-home-gallery-4.webp',
+		description: 'Makeup by Alison'
+	},
+	{
+		src: '/images/52-home-gallery-5.webp',
+		description: 'Nail art by Rebecca'
+	},
+	{
+		src: '/images/52-home-gallery-1.webp',
+		description: 'Hair coloring by Amanda'
+	},
+	{
+		src: '/images/52-home-gallery-2.webp',
+		description: 'Nail art by Rebecca'
+	},
+	{
+		src: '/images/52-home-gallery-3.webp',
+		description: 'Hair styling by Jerry'
+	},
+	{
+		src: '/images/52-home-gallery-4.webp',
+		description: 'Makeup by Alison'
+	},
+	{
+		src: '/images/52-home-gallery-5.webp',
+		description: 'Nail art by Rebecca'
+	}
+];
+
+export const OUR_SPECIALISTS = [
+	{
+		src: '/images/52-profile-1.webp',
+		name: 'Alison Cooper',
+		job: 'Makeup Artist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-2.webp',
+		name: 'Christie Weiss',
+		job: 'Hair Stylist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-3.webp',
+		name: 'Rebecca Smith',
+		job: 'Lash & Brow Artist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-4.webp',
+		name: 'Joan Robins',
+		job: 'Nail Artist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-5.webp',
+		name: 'Jessica Lin',
+		job: 'Hair Stylist',
+		link: '#'
+	},
+	{
+		src: '/images/52-profile-6.webp',
+		name: 'Mia Bennett',
+		job: 'Makeup Artist',
+		link: '#'
+	}
+];
